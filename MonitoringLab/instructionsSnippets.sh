1ff6145669972dec7ba9787beed4bf313d93121d
Julio Faerman <jfaerman@gmail.com>
2014-01-17 14:26:33 -0200

    Removing MonPack

--- MonitoringLab/instructionsSnippets.sh
+++ MonitoringLab/instructionsSnippets.sh
@@ -2,13 +2,14 @@
 # Interacting with CloudWatch   #
 # ============================= #
 
-# Topic 3.2
+# Topic ???
 # =========
 
-wget http://ec2-downloads.s3.amazonaws.com/cloudwatch-samples/CloudWatchMonitoringScripts-v1.1.0.zip
-unzip CloudWatchMonitoringScripts-v1.1.0.zip 
-rm CloudWatchMonitoringScripts-v1.1.0.zip 
-cd aws-scripts-mon
+aws cloudwatch put-metric-data \
+  --namespace Student \
+  --metric-name AttentionLevel \
+  --value 8
+
 
 
 # Topic 3.3
